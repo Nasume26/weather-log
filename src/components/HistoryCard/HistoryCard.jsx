155a673ff80b9915fff0d3febe22ded1b5ce5fd2
Maxwell Roberson <108200270+Nasume26@users.noreply.github.com>
2022-08-26 15:29:58 -0500

    Managed to parse the SQL date into JS date

--- src/components/HistoryCard/HistoryCard.jsx
+++ src/components/HistoryCard/HistoryCard.jsx
@@ -4,9 +4,18 @@ const HistoryCard = (props) => {
     const { historyData } = props;
     console.log(historyData)
 
+    const filterDate = historyData.createdAt.split("T", 1).pop()
+    const parsedDate = filterDate.split(/[- : ]/)
+
+    const timeSplit = historyData.createdAt.split("T", -1).pop()
+    const timeSplitEnd = timeSplit.split(".", 1).pop()
+    const parsedTimeSplit= timeSplitEnd.split(/[- : ]/)
+    const renderDate = new Date(Date.UTC(parsedDate[0], parsedDate[1]-1, parsedDate[2], parsedTimeSplit[0], parsedTimeSplit[1], parsedTimeSplit[2]));
+    
     return (
         <div className="history-container">
-            <h1>{historyData.createdAt}</h1>
+            <h1>{renderDate.toDateString()} </h1>
+            <p>{renderDate.toTimeString()}</p>
             <h3>{historyData.temp} degrees farenheit</h3>
             <h4>RealFeel: {historyData.feelTemp}</h4> 
             <p>{historyData.cond}</p>
