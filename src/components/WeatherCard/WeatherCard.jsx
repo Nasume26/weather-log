a5675f3c6cd6307cf4edb8517e9c375d0482c474
Maxwell Roberson <108200270+Nasume26@users.noreply.github.com>
2022-08-26 15:43:47 -0500

    Began laying out the main card

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -5,12 +5,19 @@ const WeatherCard = (props) => {
 
     return (
         <div className='weather-data'>
-            <h1>{weatherData.name}</h1>
-            <h3>{weatherData.main.temp} degrees farenheit</h3>
-            <h4>RealFeel: {weatherData.main.feels_like}</h4> 
-            <p>{weatherData.weather[0].description}</p>
-            <p>Humidity: {weatherData.main.humidity}%</p>
-
+            <div className='weather-data__top'>
+                <h1>{weatherData.name}</h1>
+                <h3>{weatherData.main.temp} degrees farenheit</h3>
+                <h4>RealFeel: {weatherData.main.feels_like}</h4> 
+            </div>
+            <div>
+                <p>{weatherData.weather[0].description}</p>
+                <p>Humidity: {weatherData.main.humidity}%</p>
+                
+            </div>
+            <div>
+            <img src= "https://openweathermap.org/themes/openweathermap/assets/img/logo_white_cropped.png" />
+            </div>
         </div>
 
     )
