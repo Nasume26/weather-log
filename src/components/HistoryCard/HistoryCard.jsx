55715477a2982bb00bcfbdf36482de2341a18e49
Maxwell Roberson <108200270+Nasume26@users.noreply.github.com>
2022-08-29 16:02:18 -0500

    Started image filters

--- src/components/HistoryCard/HistoryCard.jsx
+++ src/components/HistoryCard/HistoryCard.jsx
@@ -1,4 +1,5 @@
 import "./HistoryCard.scss"
+import scatteredClouds from '../../images/scattered clouds.png'
 
 const HistoryCard = (props) => {
     const { historyData } = props;
@@ -11,15 +12,28 @@ const HistoryCard = (props) => {
     const timeSplitEnd = timeSplit.split(".", 1).pop()
     const parsedTimeSplit= timeSplitEnd.split(/[- : ]/)
     const renderDate = new Date(Date.UTC(parsedDate[0], parsedDate[1]-1, parsedDate[2], parsedTimeSplit[0], parsedTimeSplit[1], parsedTimeSplit[2]));
+
+
+    const imageFilter = () => {
+        if (historyData.cond == "scattered clouds") {
+            return scatteredClouds
+        }
+    }
     
     return (
         <div className="history-container">
             <h1>{renderDate.toDateString()} </h1>
             <p>{renderDate.toTimeString()}</p>
-            <h3>{historyData.temp} degrees farenheit</h3>
-            <h4>RealFeel: {historyData.feelTemp}</h4> 
-            <p>{historyData.cond}</p>
-            <p>Humidity: {historyData.humidity}%</p>
+            <div>
+                <h3>{historyData.temp} degrees farenheit</h3>
+                <h4>RealFeel: {historyData.feelTemp}</h4> 
+                <p>{historyData.cond}</p>
+                <p>Humidity: {historyData.humidity}%</p>
+            </div>
+            <img src={imageFilter()}/>
+            <div>
+
+            </div>
         </div>
     )
 }
