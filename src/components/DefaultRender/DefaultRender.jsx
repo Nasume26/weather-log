98a1cc0c4da078fb6e5eb85ce8516a14ecfb6d74
Maxwell Roberson <108200270+Nasume26@users.noreply.github.com>
2022-08-30 10:58:17 -0500

    Started styling default render

--- src/components/DefaultRender/DefaultRender.jsx
+++ src/components/DefaultRender/DefaultRender.jsx
@@ -1,19 +1,22 @@
-
+import "./DefaultRender.scss"
 
 const DefaultRender = (props) => {
 
 
     return (
-            <div>
+
+        <div className="default-container">
+            <div className="default-container__main">
                  <h1>Welcome to Weather Log!</h1>
                  <p>In order to use this application, please input your desired
                     location in the search bar. Every time you search for weather data, 
                     that data is logged and stored in a database for the next
                     person to see! Please note that history data is based on your Search
-                    terms, to get the best results use CITY,STATE,COUNTRY as your Search 
+                    terms, to get the best results use <span className="default-container__main__bold">CITY,STATE,COUNTRY</span> as your Search 
                     term!
                  </p>
             </div>
+        </div>
     )
 }
 
